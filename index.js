17f73cbd1e4f3c84653640ee434cd183768193cd
Matheus Garajau <matheusgarajaucontato@gmail.com>
2026-03-12 15:39:41 -0300

    comecando javascript

---
+++ index.js
@@ -0,0 +1,15 @@
+let altura = 1.75
+let peso = 70
+
+console.log(altura)
+let imc = peso / (altura * altura) 
+
+console.log(imc) 
+
+console.log('teste')
+
+// * = multiplicação
+// / = divisão
+// + = adição
+// - = subtração
+// () = prioridade 
